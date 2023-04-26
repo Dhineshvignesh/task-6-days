e6f909fbe9c7408aa228a74d21e2bc06a600bda8
dhineshraja <127471949+Dhineshvignesh@users.noreply.github.com>
2023-04-26 10:17:09 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1 id="id01">Old Heading</h1>
+<p>dhinesh</p>
+<script>
+const element = document.getElementById("id01");
+element.innerHTML = "New Heading";
+document.write(date());
+
+</script>
+<P>Thank You</P>
+</body>
+</html>
